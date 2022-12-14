3ed3127064071250275b41c8901ca883496a9577
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-14 19:34:17 +0300

    comments in PHP

---
+++ learn2.php/learn2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!-- HTML comment ...... -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>learn2.php | comment in PHP</title>
+</head>
+<body>
+    <!-- learn2.php | comments in PHP
+        |-----------------------------
+        comment in HTML.php
+        A comment in PHP code is a line that is not executed as a 
+        part of the program. Its only purpose is to be read by 
+        someone who is looking at the code. Comments can be used 
+        to: Let others understand your code.
+        
+        In PHP there are 3 ways to comment :
+         1. #   comments 
+         2. //  comments  
+         3. /*  comments  */  
+    -->
+    <!--<h1>Learn PHP | comments in PHP</h1> -->
+  <?php
+  echo 'Learn2 echo | comments in PHP <br>';
+  # echo 'Learn2 PHP | comments in PHP';
+  # the codes with PHP will not appear in inspect dev mode over web-browsers..
+  # Security NOTE ::::::::::::::::::::::::::::::::::::::::::::::::
+  # see how the html comment lines very easy to read from anyone..
+  #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
+  //echo '<h1>echo comments by //</h1>';
+  //------------------------------------
+  
+  /* 
+  echo 'PHP <br>';
+  echo 'HTML <br>';
+  echo 'SQL';
+  */
+
+  ?>
+
+  <script> // to test js only!!!
+    alert('Welcome to PHP over HTML CLASS, <br> with love from javascript ');
+  </script>
+
+</body>
+</html>
