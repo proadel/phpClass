cd27203827cb5d2be21462cf31b2988f240a4491
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-14 16:08:27 +0300

    echo_PHP +print

---
+++ learn1.php/learn1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>learn1.php</title>
+</head>
+<body>
+    <!-- learn1.php | echo_PHP +print
+    //-------------------------------------
+      // - echo = print = display
+      // - echo "string";
+         - echo num ;
+         - <br> = break = HTML"new line"
+         - <hr> = horzintal Line = HTML
+         - echo codes ;
+         - print (codes);
+    //-------------------------------------
+     -->
+    <?php
+    echo "string echo : Lets learn PHP 2022 <hr>";
+    echo 2022,'<br>' ;
+    echo "2022 <br>";
+    echo '2022';
+    echo '<h2>welcome to PHP Class 2022</h2>';
+    echo '<h3> echo : learn PHP is easy</h3>';
+    
+    print ("print : Leats Study PHP 2022 <br>");
+
+    echo 2 + 2 ,'<br>';
+    echo "2+2 <br>";
+    echo '2+2', '<br>';
+    
+    print (2*5);
+
+    ?>
+
+</body>
+</html>
