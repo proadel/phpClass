e2b6abbe7342f8966126560e0263438c4cac2449
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-16 02:37:31 +0300

    str reverse

---
+++ learn9.php/learn9.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>learn9.php | str reverse</title>
+</head>
+<body>
+
+ <?php
+ // learn9.php | strrev — Reverse a string
+  // strrev(string $string): string
+  // https://www.php.net/manual/en/function.strrev
+  //==============================================
+  // example -------------------------------------
+  $name = "Studen";
+  $info = "student learn PHP with SQL and HTML";
+  echo strrev($name), "<br>";
+  echo strrev($info);
+  //==============================================
+ ?>
+    
+</body>
+</html>
