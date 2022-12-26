2112511579a541df6ac030fe78cd675dba303fa2
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-27 01:46:41 +0300

    PHP update SQL
    update.. edit the title

--- learn39.php/learn39.php
+++ learn39.php/learn39.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>learn38.php | PHP update SQL</title>
+    <title>learn39.php | PHP update SQL</title>
 </head>
 <body>
     <h1>learn39.php | PHP SQL update</h1> <hr><br><br>
